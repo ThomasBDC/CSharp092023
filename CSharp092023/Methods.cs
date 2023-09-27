8a8a743974803730b6bea877edd45750b31fe32f
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-09-27 10:28:10 +0200

    Methods course

---
+++ CSharp092023/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp092023
+{
+    public static class Methods
+    {
+        private static Random rnd = new Random();
+
+        private static int getNumberAlea(int MAX)
+        {
+            return rnd.Next(0, MAX);
+        }
+
+        public static string GetRandomItemInTab(List<string> baseList)
+        {
+            int indexRandom = getNumberAlea(baseList.Count());
+            string randomItem = baseList[indexRandom];
+            return randomItem;
+        }
+
+        public static string GetRandomItemInTab(string[] baseArray)
+        {
+            int indexRandom = getNumberAlea(baseArray.Count());
+            string randomItem = baseArray[indexRandom];
+            return randomItem;
+        }
+    }
+}
--- CSharp092023/Program.cs
+++ CSharp092023/Program.cs
@@ -1,63 +1,50 @@
-﻿/*
- * 
- * CONDITIONS
- *          -> éxécuter du code, en fonction d'une condition
- * 
-SI  (ET OU)
-SINON
- 
-if (&& ||)
-else
-else if 
- */
+﻿using CSharp092023;
 
-/*
- * BOUCLES
- *          -> éxécuter du code plusieurs fois, avec des conditions
- *          
- * TANT QUE (condition)
- * 
- * POUR i de 0 à x
- 
- */
-
-Console.WriteLine("Bienvenue dans notre jeu RoulettePerdante");
-
-List<string> list = new()
+List<string> listAnimals = new()
 {
     "WOUF",
     "MIAOU",
     "Quoi de neuf"
 };
 
-//On peut le faire sur une liste mais pas sur un tableau
-list.Add("Hihan");
+SayHelloToUser();
 
-Console.WriteLine("Voici les éléments que vous pouvez voir");
-
-foreach (var item in list)
-{
-    Console.WriteLine(item);
-}
-
-string reponse;
+//Début du jeu
 do
 {
-    //Ici on mettra le contenu du jeu
+    DoTourDeJeu();
+
+} while (GetReponsePlayAgain());
+
+
+
+#region Methods
+void SayHelloToUser()
+{
+    Console.WriteLine("Bienvenue dans notre jeu RoulettePerdante");
+
+    Console.WriteLine("Voici les éléments que vous pouvez voir");
+
+    foreach (var item in listAnimals)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+void DoTourDeJeu()
+{
     Console.WriteLine(" ");
     Console.WriteLine(" ");
     Console.WriteLine("Lancement de la roulette ...");
-    //Trois cases
-    // CHIEN, CHAT, LAPIN
-    Random rnd = new Random();
-    int firstCase = rnd.Next(0, list.Count()); // creates a number between 0 and 2
-    int secondCase = rnd.Next(0, list.Count());
-    int thirdCase = rnd.Next(0, list.Count());
 
+    string firstAnimal = Methods.GetRandomItemInTab(listAnimals);
+    string secondAnimal = Methods.GetRandomItemInTab(listAnimals);
+    string thirdAnimal = Methods.GetRandomItemInTab(listAnimals);
 
-    Console.WriteLine(list[firstCase] + " " + list[secondCase] + " " + list[thirdCase]);
+    Console.WriteLine(firstAnimal + " " + secondAnimal + " " + thirdAnimal);
 
-    if (firstCase == secondCase && secondCase == thirdCase)
+    if (firstAnimal == secondAnimal && secondAnimal == thirdAnimal)
     {
         Console.WriteLine("Vous avez gagné");
     }
@@ -65,8 +52,19 @@ do
     {
         Console.WriteLine("Vous avez perdu");
     }
+}
 
-    //Condition pour lancer une nouvelle partie
+bool GetReponsePlayAgain()
+{
     Console.WriteLine("Voulez-vous re-perdre à la roulette ? O/N");
-    reponse = Console.ReadLine();
-} while (reponse == "O" || reponse == "o");
+    string reponse = Console.ReadLine();
+    if(reponse == "O" || reponse == "o" || reponse == "oui")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+#endregion
